678350c19b45bc795624ac6beb37fe8ae32241d8
Slaython <slaython.ads@gmail.com>
2021-10-17 10:43:34 -0300

    alteração

--- area-triangulo.js
+++ area-triangulo.js
@@ -2,8 +2,8 @@ import entradaDados from 'readline-sync';
 
 console.log("Área do triângulo retângulo: \n");
 
-let base = entradaDados.question("Informe a base: ");
-let altura = entradaDados.question("Informe a altura: ");
+let base = Number(entradaDados.question("Informe a base: "));
+let altura = Number(entradaDados.question("Informe a altura: "));
 
 let area = (base * altura) / 2;
 
